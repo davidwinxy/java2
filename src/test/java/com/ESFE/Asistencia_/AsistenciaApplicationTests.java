2f62d5f7f4b7e082df952a9fddcb310619956556
davidwinxy <daviddiaz04d@gmail.com>
2024-07-29 16:49:35 -0600

    commit uno

---
+++ src/test/java/com/ESFE/Asistencia_/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESFE.Asistencia_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
